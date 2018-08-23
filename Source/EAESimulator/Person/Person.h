6d626baac9555ed1f35519f23febd42d9813c888
Zhitao <31802he@163.com>
2018-08-23 11:47:23 -0600

    1

---
+++ Source/EAESimulator/Person/Person.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class EAESIMULATOR_API Person
+{
+public:
+	Person();
+	~Person();
+
+private:
+	//shit
+};
