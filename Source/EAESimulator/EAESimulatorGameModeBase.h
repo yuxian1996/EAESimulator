428e977d4b7e9d6789475d5275c132963b2e571f
yuxian <yuxian312@gmail.com>
2018-08-23 10:44:13 -0600

    Initial commit

---
+++ Source/EAESimulator/EAESimulatorGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EAESimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EAESIMULATOR_API AEAESimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
