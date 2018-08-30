eb351029cd4a5c2ef916aa5cbb0ae8f74b74647a
Yuxian Deng <u1166596@utah.edu>
2018-08-30 00:50:35 -0600

    Update UI

--- Source/EAESimulator/GameLoop/HumanManager.cpp
+++ Source/EAESimulator/GameLoop/HumanManager.cpp
@@ -5,12 +5,13 @@
 FString UHumanManager::names[8] = { "Nash", "David", "Liang", "Sally", "Natalia", "Ashley", "Bob", "Ryan "};
 FString UHumanManager::descriptions[8] = { "Nash is a nerd which is interested in art.\nHis favorite game is Call of Unity III. \nHis dream job is art director in Biozzard.", 
 "David has math background and wants \nto become a technical artist. \nHis favorite game is Overshoot.", 
-"Liang believes the best game should be good looking. \nHis favorite game is Living Cell. \nHe has very strong art background.",
-"Sally wants to be a game producer so that she \ncan make a very cool game which the player can control a huge dinosaur to create disaster.", 
-"Natalia’s favorite game is  Hiscraft. \nShe wants to be a game engineer.", 
-"Ashley has strong background in game user research. \nShe can improve students’ production skill very well.", 
-"Bob is a good at engineering. He worked for Biozzard more than 10 years. \nHe have a lot of social link with game industry.", 
-"Ryan is a humorous technical artist. \nHe has more than 10 years experiences work with big team."};
+"Liang believes the best game should be \ngood looking. His favorite game is Living \nCell. He has very strong art background.",
+"Sally wants to be a game producer so \nthat she can make a very cool game \nwhich the player can control a \nhuge dinosaur to create disaster.", 
+"Natalia is favorite game is  Hiscraft. \nShe wants to be a game engineer.", 
+"Ashley has strong background in game \nuser research. \nShe can improve students' \nproduction skill very well.", 
+"Bob is a good at engineering. He worked \nfor Biozzard more than 10 years. He have \na lot of social link with game industry.", 
+"Ryan is a humorous technical artist. \nHe has more than 10 years experiences \nwork with big team."};
+FString UHumanManager::AvatarPath = "/Game/Arts/UI/Person/Avatar/";
 
 UHumanManager::UHumanManager()
 {	
@@ -25,6 +26,10 @@ UHumanManager::UHumanManager()
 		f.Program = 30;
 		f.TechArt = 30;
 		s->SetCapacity(f);
+		FString texName = FString::FromInt(i + 1);
+		FString name = AvatarPath + texName;
+
+		s->LoadAvatar(name);
 		m_StudentGroup.Add(s);
 	}
 
@@ -39,6 +44,9 @@ UHumanManager::UHumanManager()
 		f.Program = 150;
 		f.TechArt = 150;
 		s->SetCapacity(f);
+		FString texName = "p_" + FString::FromInt(i + 1);
+		FString name = AvatarPath + texName;
+		s->LoadAvatar(name);
 		m_ProfessorGroup.Add(s);
 	}
 
